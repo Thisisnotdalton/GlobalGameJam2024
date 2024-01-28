fc91f6a918322a8221ff60fd6910ece000fa2317
Michelle Kim <mkimatlanta@gmail.com>
2024-01-27 23:28:08 -0500

    Desktop Art OVerhaul
    my eyes hurt

--- Scripts/Core/ExclusiveStateNode.cs
+++ Scripts/Core/ExclusiveStateNode.cs
@@ -2,47 +2,47 @@ using Godot;
 
 namespace GlobalGameJam2024.Scripts.Core
 {
-    public class ExclusiveStateNode : Node
-    {
-        [Export] private Resource _stateIdentifier = null;
+	public class ExclusiveStateNode : Node
+	{
+		[Export] private Resource _stateIdentifier = null;
 
-        [Signal]
-        private delegate void OnStateChanged(bool enabled);
+		[Signal]
+		private delegate void OnStateChanged(bool enabled);
 
-        public void TriggerStateChange(bool enabled)
-        {
-            EmitSignal("OnStateChanged", enabled);
-        }
+		public void TriggerStateChange(bool enabled)
+		{
+			EmitSignal("OnStateChanged", enabled);
+		}
 
-        public Resource StateIdentifier
-        {
-            get { return _stateIdentifier; }
-        }
+		public Resource StateIdentifier
+		{
+			get { return _stateIdentifier; }
+		}
 
-        public static string GetStateIDForResource(Resource resource)
-        {
-            return resource.ResourcePath;
-        }
+		public static string GetStateIDForResource(Resource resource)
+		{
+			return resource.ResourcePath;
+		}
 
 
-        public static string GetStateDisplayNameForResource(Resource resource)
-        {
-            if (resource.Get("stateID") is string stateID && stateID.Length > 0)
-            {
-                return stateID;
-            }
+		public static string GetStateDisplayNameForResource(Resource resource)
+		{
+			if (resource.Get("stateID") is string stateID && stateID.Length > 0)
+			{
+				return stateID;
+			}
 
-            return GetStateIDForResource(resource);
-        }
+			return GetStateIDForResource(resource);
+		}
 
-        public string GetStateID()
-        {
-            return GetStateIDForResource(_stateIdentifier);
-        }
+		public string GetStateID()
+		{
+			return GetStateIDForResource(_stateIdentifier);
+		}
 
-        public string GetStateDisplayName()
-        {
-            return GetStateDisplayNameForResource(_stateIdentifier);
-        }
-    }
+		public string GetStateDisplayName()
+		{
+			return GetStateDisplayNameForResource(_stateIdentifier);
+		}
+	}
 }
