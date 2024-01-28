71bec4875f40f3b3f684c2942bcc85533631b4ea
Michelle Kim <mkimatlanta@gmail.com>
2024-01-28 00:30:01 -0500

    My contrinbuteiojns to the msuic and the cresdits page
    My fingers arecold :)

--- Scripts/UI/StateChangeButton.cs
+++ Scripts/UI/StateChangeButton.cs
@@ -4,19 +4,19 @@ using Godot;
 
 namespace GlobalGameJam2024.Scripts.UI
 {
-    public class StateChangeButton : Button
-    {
-        [Export] private readonly Resource _targetStateIdentifier = null;
-        
-        public override void _Pressed()
-        {
-            base._Pressed();
-            ExclusiveStateNodeManager stateManager = SearchNodeType.FindParentOfType<ExclusiveStateNodeManager>(this);
-            if (stateManager == null)
-            {
-                throw new Exception($"No {nameof(ExclusiveStateNodeManager)} could be found as parent of {Name}!");
-            }
-            stateManager.ChangeState(_targetStateIdentifier);
-        }
-    }
+	public class StateChangeButton : Button
+	{
+		[Export] private readonly Resource _targetStateIdentifier = null;
+		
+		public override void _Pressed()
+		{
+			base._Pressed();
+			ExclusiveStateNodeManager stateManager = SearchNodeType.FindParentOfType<ExclusiveStateNodeManager>(this);
+			if (stateManager == null)
+			{
+				throw new Exception($"No {nameof(ExclusiveStateNodeManager)} could be found as parent of {Name}!");
+			}
+			stateManager.ChangeState(_targetStateIdentifier);
+		}
+	}
 }
